317167bf2b00f4e3a29371549f9d613954c12c59
Hunter Wong <hjwong@bluejeans.com>
2016-04-04 12:18:21 -0700

    more detailed responses

--- lib/hjwgem.rb
+++ lib/hjwgem.rb
@@ -3,21 +3,26 @@
 class Hjwgem
   def self.winner a_odds=50, number_of_games=3
     a_wins = 0
-    b_wins = 0
-    while a_wins < (number_of_games+1)/2 && b_wins < (number_of_games+1)/2
+    z_wins = 0
+    game_number = 1
+    while a_wins < (number_of_games+1)/2 && z_wins < (number_of_games+1)/2
       result = Random::rand 100
-      puts result
       if result < a_odds
         a_wins += 1
+        puts "A wins game #{game_number} (#{result})"
       else
-        b_wins += 1
+        z_wins += 1
+        puts "Z wins game #{game_number} (#{result})"
       end
+      game_number += 1
     end
-    if a_wins > b_wins
-      puts "a wins #{a_wins} games to #{b_wins}"
+    if a_wins > z_wins
+      puts "A wins #{a_wins} games to #{z_wins}"
+      winner = 'A'
     else
-      puts "b wins #{b_wins} games to #{a_wins}"
+      puts "Z wins #{z_wins} games to #{a_wins}"
+      winner = 'Z'
     end
-
+    winner
   end
 end
